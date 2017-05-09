ceaf28a82381755a0ce775d6d071ef432d518610
Chris Kooistra <ckooistra@gmail.com>
2017-05-09 09:25:03 -0400

    Started final report creation

--- checkMutations.py
+++ checkMutations.py
@@ -3,23 +3,60 @@
 from collections import Counter
 import re
 
+def makeCodon(seq, p):
+    if position % 3 == 0:
+        return seq[p-2:p+1]
+    if position % 3 == 1:
+        return seq[p:p+3]
+    if position % 3 == 2:
+        return seq[p-1:p+2]
+
+def makeMutatntCodon(codon, pos, mutation):
+    muantCodon = ""
+    for x in range(len(codon)):
+        if x == pos-1:
+            muantCodon += mutation
+        else:
+            muantCodon += codon[x]
+    return [codon, muantCodon]
+
+
+
+
+
+sequences = {}
+
+# Get files into memory
+with open('tmp', 'r') as ph:
+    ph.readline()
+
+    for ligne in ph:
+        s = ligne.split(" ")
+        ec = s[0].strip()
+        seq = s[1].strip()
+
+        sequences[ec] = seq
 
 with open('CosmicMutantExportSilent.tsv', 'r') as f:
 
     number = 0
     matchNumber = 0
     unMatched = 0
-    cont = 0
     report = []
+    skipped = []
+
+
     for line in f:
+        if number is 1000:
+            break
         values = line.split("\t")
 
         report = []
         number += 1
 
-        if number == 1000:
-            break
-        if values[25] == 'y' or values[23] == '' or re.search('_', values[17]):
+        if values[25] == 'y' or re.search('_', values[17]):
+
+            skipped.append(values[1])
             continue
 
         ensemblCode = values[1]
@@ -27,42 +64,24 @@ with open('CosmicMutantExportSilent.tsv', 'r') as f:
         normalNuc = values[17][-3]
         mutantNuc = values[17][-1]
         length = int(values[2])
-        genome = values[23].split(':')[0]
-        genomePosition = int(values[23].split('-')[1])
 
 
-        matched = False
+        for code in sequences:
+            if ensemblCode in code and len(sequences[code]) == length:
+                codons = makeMutatntCodon(makeCodon(sequences[code], position),position % 3, mutantNuc)
+                report.append(ensemblCode+","+str(position)+","+str(position%3)+","+ normalNuc +"," + mutantNuc + "," + codons[0]+ "," + codons[1])
 
-        with open('tmp', 'r') as ph:
-            ph.readline()
-
-            for ligne in ph:
-                sequences = ligne.split(" ")
-                seq = sequences[1].strip()
-
-                if (len(seq) < length):
-                    continue
-
-                code = sequences[0].split(".", 1)[0]
-                # print (sequences[0])
-
-                if (code == ensemblCode and seq[position-1] == normalNuc):
-
-                    matchNumber += 1
-                    matched = True
+    if number % 200 == 0:
+        print(number)
 
 
-        if not(matched):
-            unMatched += 1
-            print("unMatched position = "+str(position)+"\n ensemblCode ="+ensemblCode+"\n ")
-
-        if number % 200 == 0:
-            print(number)
 
 
-with open ('res.txt', 'w') as f:
+with open ('report.csv', 'w') as fil:
+    fil.write('ENSEMBL,Position,Nuc,mNuc,Codon,Codon Position,AA\n')
+    for line in report:
+        fil.write(line)
 
-    f.write("Total = "+str(number)+'\n')
-    f.write("Matched = "+ str(matchNumber)+'\n')
-    f.write("Un Matched = "+ str(unMatched)+'\n')
-    f.write("Match percentage = "+str(matchNumber/(matchNumber+unMatched))+'\n')
+with open ('skippedReport.txt', 'w') as fil:
+    for line in skipped:
+        fil.write(line+"\n")
