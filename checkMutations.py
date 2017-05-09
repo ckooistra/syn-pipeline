d98ad6fe150fa05bd5e1cf8981a4972278e2387c
Chris Kooistra <ckooistra@gmail.com>
2017-05-09 12:32:32 -0400

    Comitting changes before changing dic.get structure

--- checkMutations.py
+++ checkMutations.py
@@ -14,26 +14,22 @@ def makeCodon(seq, p):
 def makeMutatntCodon(codon, pos, mutation):
     muantCodon = ""
     for x in range(len(codon)):
-        if x == pos-1:
+        if x == 3-(pos+1):
             muantCodon += mutation
         else:
             muantCodon += codon[x]
     return [codon, muantCodon]
 
-
-
-
-
 sequences = {}
 
-# Get files into memory
+# Get referemce CDS files into memory as dictionary
 with open('tmp', 'r') as ph:
     ph.readline()
 
     for ligne in ph:
         s = ligne.split(" ")
-        ec = s[0].strip()
         seq = s[1].strip()
+        ec = s[0].strip()+"_"+str(len(seq))
 
         sequences[ec] = seq
 
@@ -47,38 +43,41 @@ with open('CosmicMutantExportSilent.tsv', 'r') as f:
 
 
     for line in f:
-        if number is 1000:
+        if number == 1000:
             break
+
         values = line.split("\t")
 
-        report = []
         number += 1
 
         if values[25] == 'y' or re.search('_', values[17]):
-
             skipped.append(values[1])
             continue
+        try:
+            ensemblCode = values[1]
+            position = int(values[17][2:-3])
+            normalNuc = values[17][-3]
+            mutantNuc = values[17][-1]
+            length = int(values[2])
 
-        ensemblCode = values[1]
-        position = int(values[17][2:-3])
-        normalNuc = values[17][-3]
-        mutantNuc = values[17][-1]
-        length = int(values[2])
-
+        except ValueError:
+            print ("line with value error")
+            print (values[17][2:-3])
+            print("-----------------------------")
 
         for code in sequences:
             if ensemblCode in code and len(sequences[code]) == length:
                 codons = makeMutatntCodon(makeCodon(sequences[code], position),position % 3, mutantNuc)
-                report.append(ensemblCode+","+str(position)+","+str(position%3)+","+ normalNuc +"," + mutantNuc + "," + codons[0]+ "," + codons[1])
+                report.append(ensemblCode+","+str(position)+","+str(3-(position%3))+","+ normalNuc +"," + mutantNuc + "," + codons[0]+ "," + codons[1]+"\n")
 
-    if number % 200 == 0:
-        print(number)
+        if number % 200 == 0:
+            print(number)
 
 
 
 
 with open ('report.csv', 'w') as fil:
-    fil.write('ENSEMBL,Position,Nuc,mNuc,Codon,Codon Position,AA\n')
+    fil.write('ENSEMBL,CDS Position,nucOfCodon,nuc,mNuc,nCodon,mCodontion\n')
     for line in report:
         fil.write(line)
 
