3dbdec5c282f71861804af174d81751811757116
Chris Kooistra <ckooistra@gmail.com>
2017-07-13 18:00:06 -0400

    Moved Directories

--- checkMutations2.py
+++ checkMutations2.py
@@ -1,1 +1,169 @@
-/home/chris/Dropbox/BIN 3005/python_files/checkMutations2.py
+
+#/bin/python
+
+from collections import Counter
+import re
+
+def main():
+    
+    s = getSequencesFromReference()
+    getMutationInfo(s)
+
+
+def makeCodon(seq, p):
+    if p% 3 == 0:
+        return seq[p-3:p]
+    if p% 3 == 1:
+        return seq[p-1:p+2]
+    if p% 3 == 2:
+        return seq[p-2:p+1]
+
+def makeMutatntCodon(codon, pos, mutation):
+
+    pos = pos % 3
+    muantCodon = ""
+    
+    if pos is 0:
+        return codon[0:2]+mutation
+    if pos is 1:
+        return mutation+codon[1:]
+    if pos is 2:
+        return codon[0]+mutation+codon[1]
+
+def transitionOrTransversion(normalNuc, mutantNuc):
+    purines = ["A", "G"]
+    pyrimidines = ["T", "C"]
+
+    if (normalNuc in purines and mutantNuc in purines) or (normalNuc in pyrimidines and mutantNuc in pyrimidines):
+        return "transition"
+    else:
+        return "transversion"
+
+def positionOfMutation(pos, seqLength):
+   return pos/seqLength
+
+# Get sequences and return dictionary with python CDS sequences
+# Sequence ENSEMBL ID and length....ENSxxxxx_length
+def getSequencesFromReference():
+    sequences = {}
+    
+    with open('tmp', 'r') as f:
+        f.readline()
+
+        for line in f:
+            
+            s = line.split(" ")
+            seq = s[1].strip()
+            ec = s[0].strip().split(".")[0]+"_"+str(len(seq))
+
+            sequences[ec] = seq
+    
+    return sequences
+
+def reportWriter(name, title, info):
+    with open(name, 'w') as f:
+        print(title, file=f)
+        for line in info:
+            print(line, file=f)
+
+def getMutationInfo(sequences):
+    
+    with open('CosmicMutantExportSilent.tsv', 'r') as f:
+
+        number = 0
+        matchNumber = 0
+        unMatched = 0
+        report = []
+        skipped = []
+        cnt = Counter()
+        mod = []
+
+        n = 0
+        for line in f:
+
+            values = line.split("\t")
+
+            number += 1
+            # If known snip, or if insertion, stop codon or if sequence change not known skip
+            if values[25] == 'y' or re.search('_', values[17]) or values[17] == 'c.?' or re.search('\*|p.*X', values[18]):
+
+                skipped.append(values[1]+", not valid")
+                unMatched += 1
+                continue
+
+            try:
+                gene = values[0].strip()
+                ensemblCode = values[1]
+                position = int(values[17][2:-3]) #Position of the mutation
+                normalNuc = values[17][-3]
+                mutantNuc = values[17][-1]
+                length = int(values[2])
+                genome = int(values[23].split(":")[0])
+                genomePosition = int(values[23].split("-")[1])
+                aa = values[18][-1]
+
+            except ValueError:
+                skipped.append(ensemblCode+",missing genome position")
+                unMatched += 1
+                continue
+
+            code = ensemblCode+"_"+str(length)
+            if sequences.get(code):
+                if len(sequences.get(code))%3 != 0:
+                    mod.append("Ensembl code = "+code+"First 10 nucs = "+sequences[code][0:10]+"+Last 5 nucs = "+sequences[code][-10:])
+
+                codons = []
+                codons.append(makeCodon(sequences[code], position))
+                codons.append(makeMutatntCodon(codons[0], position, mutantNuc))
+                report.append(gene+','+ensemblCode+","+str(genome)+","+str(genomePosition)+
+                ","+str(len(sequences[code]))+","+str(position)+","+str(3-((position)%3))+
+                ","+ normalNuc +"," + mutantNuc + "," + codons[0]+ ","+codons[1]+","+
+                transitionOrTransversion(normalNuc,mutantNuc)+","+
+                str(round(positionOfMutation(position, length),3))+","+aa+"\n")
+
+
+                matchNumber += 1
+                cnt[gene] += 1
+
+                if aa == '*' and len(sequences.get(code))%3 is not 0:
+                    print("Code is "+code+" modulo 3 "+str(len(sequences.get(code))%3))
+
+            else:
+                s = ensemblCode +' '+ str(length)+' '
+                keys = [key for key, value in sequences.items() if ensemblCode in
+                 key.upper()]
+                for k in keys:
+                 s += '---'+k+' '+str(len(sequences[k]))
+
+                skipped.append(s)
+
+            if number % 200 == 0:
+                print(number)
+
+
+    print("n(number of items that did not work out to modulo 0 but the coding\
+          sequence was not there)= "+str(n))
+    print ("Matched number = "+str(matchNumber))
+    print ("unMatched number = "+str(unMatched))
+    print ("Match percentage = "+str((matchNumber/(unMatched+matchNumber)*100)))
+
+
+    with open ('report.csv', 'w') as fil:
+        fil.write('gene,ENSEMBL,Genome,Genome Position,LengthTranscript,CDS Position,nucOfCodon,nuc,mNuc,nCodon,mCodontion,TransitionOrTransversion,position in AA chain %,AA\n')
+        for line in report:
+            fil.write(line)
+
+    with open ('skippedReport.txt', 'w') as fil:
+        for line in skipped:
+            fil.write(line+"\n")
+
+    with open ("geneCount.txt", 'w') as fil:
+        for key in cnt.most_common():
+            fil.write(key[0]+" "+str(key[1])+'\n')
+
+    with open ("notMod.txt", 'w') as fil:
+            for line in mod:
+                        fil.write(line+'\n')
+
+if __name__ == "__main__":
+    main()
--- generateMutations.py
+++ generateMutations.py
@@ -1,1 +1,249 @@
-/home/chris/Dropbox/BIN 3005/python_files/generateMutations.py
+#!usr/bin/env python
+
+import random 
+import numpy as np
+from collections import Counter
+from checkMutations2 import makeCodon, makeMutatntCodon, getSequencesFromReference, transitionOrTransversion, reportWriter 
+from anno import opt, optChange
+import time
+
+def main():
+    
+    seq = getSequencesFromReference()
+    mutations = getMutations()
+    tic = time.clock()
+    generateSynonymousMutations(1000000, seq, mutations)
+    toc = time.clock()
+    print('time to complete 1000000 = '+str((toc-tic)/60))
+
+
+synAA = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A',
+       'CGA':'R','CGT':'R','CGC':'R','CGG':'R','AGA':'R','AGG':'R',
+       'AAT':'N','AAC':'N','GAT':'D','GAC':'D','TGT':'C','TGC':'C','CAA':'Q','CAG':'Q','GAA':'E','GAG':'E',
+       'GGA':'G','GGT':'G','GGC':'G','GGG':'G','CAT':'H','CAC':'H','ATT':'I','ATC':'I','ATA':'I',
+       'ATG':'-','TTA':'L','TTG':'L','CTA':'L','CTT':'L','CTC':'L','CTG':'L','AAA':'K','AAG':'K','TTT':'F','TTC':'F',
+       'CCA':'P','CCT':'P','CCC':'P','CCG':'P','AGT':'S','AGC':'S','TCA':'S','TCT':'S','TCC':'S','TCG':'S',
+       'ACA':'T','ACT':'T','ACC':'T','ACG':'T','TGG':'W','TAT':'Y','TAC':'Y',
+       'GTA':'V','GTT':'V','GTC':'V','GTG':'V', 'TAA':'ST', 'TGA':'ST', 'TAG':'ST'
+      }
+
+synGroup = { 'A':['GCT', 'GCC', 'GCA', 'GCG'], 'R':['CGA','CGT','CGC','CGG','AGA','AGG'],
+            'N':['AAT','AAC'], 'D':['GAT','GAC'], 'C':['TGT','TGC'], 'Q':['CAA','CAG'], 'E':['GAA','GAG'],
+            'G':['GGA','GGT','GGC','GGG'], 'H':['CAT','CAC'], 'I':['ATT','ATC','ATA'], 'M':['ATG'],
+            'L':['TTA','TTG','CTA','CTT','CTC','CTG'], 'K':['AAA','AAG'], 'F':['TTT','TTC'],
+            'P':['CCA','CCT','CCC','CCG'], 'S':['AGT','AGC','TCA','TCT','TCC','TCG'],
+            'T':['ACA','ACT','ACC','ACG'], 'W':['TGG'], 'Y':['TAT','TAC'], 'V':['GTA','GTT','GTC','GTG'],
+            'ST':['TAA','TAG','TGA'] 
+           }
+
+
+codons = ['AAA', 'AAC', 'AAG', 'AAT', 'ACA', 'ACC', 'ACG', 'ACT', 'AGA', 'AGC', 'AGG', 'AGT', 'ATA', 'ATC', 'ATG', 'ATT', 'CAA', 'CAC', 'CAG', 'CAT', 'CCA', 'CCC', 'CCG', 'CCT', 'CGA', 'CGC', 'CGG', 'CGT', 'CTA', 'CTC', 'CTG', 'CTT', 'GAA', 'GAC', 'GAG', 'GAT', 'GCA', 'GCC', 'GCG', 'GCT', 'GGA', 'GGC', 'GGG', 'GGT', 'GTA', 'GTC', 'GTG', 'GTT', 'TAA', 'TAC', 'TAG', 'TAT', 'TCA', 'TCC', 'TCG', 'TCT', 'TGA', 'TGC', 'TGG', 'TGT', 'TTA', 'TTC', 'TTG', 'TTT']
+
+
+def createCodonLists():
+    l = []
+    for i in range(64):
+        l.append([0] * 64)
+    
+    return l
+        
+def checkCodonDifferences(c1, c2):
+    
+    rep = {}
+    l1 , l2 = list(c1), list(c2)
+
+    for i in range(3):
+        if l1[i] != l2[i]:
+            rep[i] = [l1[i], l2[i], transitionOrTransversion(l1[i], l2[i])]
+    return rep
+
+
+def listNormalizer(l):
+
+    a = []
+    
+    for row in l:
+        newArr = []
+
+        for elem in row:
+            if sum(row)!= 0:
+                newArr.append(elem/sum(row))
+            else:
+                newArr.append(elem)
+        a.append(newArr)
+    
+    return a 
+
+
+def createMutationProbMatrix(transi, transv):
+    
+    lists = createCodonLists()
+    t = {'transition':transi, 'transversion':transv}
+    for i in range(64):
+        c1 = codons[i]
+        
+        for j in range(64):
+            c2 = codons[j]
+            differences = checkCodonDifferences(c1,c2)
+            if c2 == c1 or synAA[c2] != synAA[c1]:
+                lists[i][j] = 0
+            
+            else:
+                prob = 1.0
+                for key in differences:
+                    prob *= t[differences[key][2]] 
+                lists[i][j] = prob
+
+    lists = listNormalizer(lists)
+
+    a  = np.array(lists)
+
+    return (a)
+
+
+
+def getMutations():
+
+    cntGene = Counter()
+    cntTran = Counter()
+    
+    tran = set() 
+
+
+    with open('report.csv', 'r') as f:
+        f.readline()
+
+        for line in f:
+            words = line.split(',')
+            
+            gene = words[0]
+            transcript = words[1]+'_'+words[4]
+
+            cntGene[gene] += 1
+            cntTran[transcript] += 1
+            
+            tran.add(transcript)
+    
+    return [tran, cntTran, cntGene]    
+
+
+def codonWeightedChoices(probVector, probNonZero):
+    
+    l =[[],[]]
+    
+     
+    #Loop over indices in probability vector
+    for codonIndex in probNonZero:
+        codon = codons[codonIndex]
+        probability = probVector[codonIndex]
+        l[0].append(codon)
+        l[1].append(probability)
+
+    
+    return [tuple(l[0]), tuple(l[1])]
+
+ 
+
+def checkMutationType(wc,mc):
+    
+    
+    if synAA[wc] == synAA[mc]:
+        return True
+    else:
+        return False
+    
+
+
+
+def generateRandomMutations(number, sequences, transcripts):
+    
+    report = []
+    nucs = np.array(['A', 'T', 'C', 'G'])
+    noomber = 0
+    while number > 0:
+
+        options = []
+        # Choose random CDS from pool 
+        t = random.sample(transcripts[0], 1)[0]
+
+        # Choose random nucleotide in chosen sequence
+        i = random.randint(0,(len(sequences[t])-1))
+         
+        # Chosen nucleotide stored as n.
+        n = sequences.get(t)[i]
+        
+        # Wild type codon
+        wc = makeCodon(sequences.get(t), i+1)
+
+        
+        #If Codon or sequence pulled are not mod3 or contain 'N' value nucleotides
+        if len(wc)<3 or len(sequences.get(t)) % 3 != 0 or 'N' in sequences.get(t):
+            continue
+
+        
+        options = nucs != n
+        
+        mutantNuc = random.choice(nucs[options])
+        mc = makeMutatntCodon(wc, i+1, mutantNuc)
+        
+
+        if wc == 'TGA' or wc == 'TAA' or wc == 'TAG' or mc == 'TGA' or mc == 'TAA' or mc == 'TAG':
+            continue
+        
+        oc = optChange(wc, mc)
+        tOrt = transitionOrTransversion(n, mutantNuc)
+        if checkMutationType(wc, mc):
+            report.append(wc+','+mc+','+tOrt+','+str.format('{0:.3f}',oc))
+            number -= 1
+        noomber += 1    
+
+    print('number of iterations '+str(noomber))
+    
+    with open('generatedMutations.csv', 'w') as f:
+        print('WC,MC,TransitionOrTransversion,OC', file = f)
+        for line in report:
+            print(line, file = f)
+
+def generateSynonymousMutations(number, sequences, transcripts):
+    
+    report = []
+    
+    prob = createMutationProbMatrix(0.3,0.1)
+
+    while number > 0:
+        # Choose random CDS from pool 
+        t = random.sample(transcripts[0], 1)[0]
+
+        # Choose random nucleotide in chosen sequence
+        i = random.randint(0,(len(sequences[t])-1))
+         
+        # Chosen nucleotide stored as n.
+        n = sequences.get(t)[i]
+        
+        # Wild type codon
+        wc = makeCodon(sequences.get(t), i+1)
+
+        #If Codon or sequence pulled are not mod3 or contain 'N' value nucleotides
+        if len(wc)<3 or len(sequences.get(t)) % 3 != 0 or 'N' in sequences.get(t) or wc == 'ATG' or synAA[wc] == 'ST' or wc == 'TGG':
+            continue
+
+        #Get Probability Vector from Probability Matrix for wildtype codon
+        probVector = prob[codons.index(wc)]
+        
+        #Get indices of values that are not zero
+        nonzeros = np.nonzero(probVector)[0]
+
+        #Probabilities for given WildType codon (list l with codons l[0] and probabilities l[1])
+        probsForWildTypeCodon = codonWeightedChoices(probVector, nonzeros)
+
+        mc = np.random.choice(probsForWildTypeCodon[0], p=probsForWildTypeCodon[1])
+        
+        oc = optChange(wc, mc)
+
+        report.append(wc+','+mc+','+str.format('{0:.3f}',oc)) 
+        number -= 1
+
+    reportWriter('generatedSynMutationsWithProb.csv', 'WC,MC,OC', report) 
+
+if __name__ == "__main__":
+    main()
